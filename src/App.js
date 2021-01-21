ead2b26c1afbcfcce0210d8b2a859e49c1c178ba
alexanderalban <albanalexander@gmail.com>
2021-01-21 08:20:02 -0600

    version 2 works, individual users are opening

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import ContactCard from './ContactCard';
+
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      arrayOfContacts: [],
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api?results=25')
+      .then( res => {
+        const arrayOfContacts = res.data.results
+        console.log("axios", arrayOfContacts)
+        this.setState({ arrayOfContacts })
+      })
+  }
+
+  render() {
+        return (
+          <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {console.log("results", this.state.arrayOfContacts)}
+        <div>
+        <ol>{this.state.arrayOfContacts.map((contact, index) => {
+          return (
+            
+            <ContactCard key={index}
+            firstName={contact.name.first}
+            lastName={contact.name.last}
+            picture={contact.picture.large}
+            phone={contact.phone}
+            cell={contact.cell}
+            email={contact.email}
+            city={contact.location.city}
+            state={contact.location.state}
+            country={contact.location.country}
+            >
+          </ContactCard>
+        )  
+      })}</ol>
+      </div>
       </header>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/ContactCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class ContactCard extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("PROPS", this.props)
+
+        this.state = {
+            isHidden: false
+        }
+    }
+        
+        
+        
+        
+        detailToggle = () => {
+            let hidden = this.state.isHidden;
+            if (hidden === true) {
+                this.setState({isHidden: false})
+            } else {
+                this.setState({isHidden: true})
+            }
+            
+        };
+        
+        
+        render() {
+            if (this.state.isHidden === true) {
+                console.log(this.state.isHidden)
+                return (
+                    <section>
+                    <li style={{ margin: "0px"}}>
+                        <img src={this.props.picture} alt={"Person"}></img>
+                        <h2>{this.props.firstName} {this.props.lastName}</h2>
+                        <h4>Phone: {this.props.phone}</h4>
+                            <h4>Cell: {this.props.cell}</h4>
+                          <h4>{this.props.email}</h4>
+                        <h5>{this.props.city}, {this.props.state}, {this.props.country}</h5>
+                    </li>
+                    <button style={{ margin: "10px"}} onClick={this.detailToggle}>
+                       Hide Details
+                    </button>
+                </section>
+                )
+            } else {
+                return (
+                    <section>
+                    <li style={{ margin: "0px"}}>
+                         <img src={this.props.picture} alt={"Person"}></img>
+                          <h2>{this.props.firstName} {this.props.lastName}</h2>
+                       </li>
+                    <button style={{ margin: "10px"}} onClick={this.detailToggle}>
+                        Show Details</button>
+                </section>
+                )
+            }
+        }
+}
+export default ContactCard;
